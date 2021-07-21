c2aa88af47c14e8d2fa80243139cb8573c4e0368
nvdinh98 <86923066+nvdinh98@users.noreply.github.com>
2021-07-21 08:34:52 +0700

    update

---
+++ demo.js
@@ -0,0 +1,19 @@
+function check(n) {
+    if ( n < 2) {
+        return false
+    }
+    for ( let i = 2 ; i < n; i++)
+        if ( n % i == 0) {
+            return false
+        }
+    return true
+
+}
+let number = 0;
+while (number < 1000) {
+    if(check(number)) {
+        document.write(number + "<br>")
+    }
+    number ++ ;
+}
+
